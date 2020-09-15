347175d92a0dc24bbb1a2ce341d2e7911ce40085
env0 <contact@env0.com>
2020-09-15 17:41:32 +0300

    Brew formula update for terratag version v0.1.57

--- terratag.rb
+++ terratag.rb
@@ -2,12 +2,12 @@
 class Terratag < Formula
   desc "Automatically create and maintain tags across their entire set of AWS, Azure, and GCP resources on Terraform"
   homepage "https://github.com/env0/terratag"
-  version "0.1.56"
+  version "0.1.57"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/env0/terratag/releases/download/v0.1.56/terratag_0.1.56_darwin_amd64.tar.gz"
-    sha256 "435328934537e550612f0c5ca2f11de6e4c4f58dabff5c38a6c3b3f5e5f9f516"
+    url "https://github.com/env0/terratag/releases/download/v0.1.57/terratag_0.1.57_darwin_amd64.tar.gz"
+    sha256 "bcd57e1a8036633e46e20befdca5d218e52d34bee425d308457b6f66d584ddb3"
   elsif OS.linux?
   end
 
