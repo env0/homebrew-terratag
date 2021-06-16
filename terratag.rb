31c7f913fcc61ed18faef9620813ece86d00af4d
env0 <contact@env0.com>
2021-06-16 11:32:15 +0300

    Brew formula update for terratag version v0.1.29

--- terratag.rb
+++ terratag.rb
@@ -5,16 +5,21 @@
 class Terratag < Formula
   desc "Automatically create and maintain tags across their entire set of AWS, Azure, and GCP resources on Terraform"
   homepage "https://github.com/env0/terratag"
-  version "0.1.28"
+  version "0.1.29"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/env0/terratag/releases/download/v0.1.28/terratag_0.1.28_darwin_amd64.tar.gz"
-    sha256 "3879dec0b2709949bb29d9458c0377e9bdba0836943bd057054ceb6321bd8565"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/env0/terratag/releases/download/v0.1.29/terratag_0.1.29_darwin_amd64.tar.gz"
+      sha256 "a3d64e16363ef200dbe259b56f8a887c1231eb891b2e30cbf275cdab018e95f7"
+    end
   end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/env0/terratag/releases/download/v0.1.28/terratag_0.1.28_linux_amd64.tar.gz"
-    sha256 "2164598e91bf662955d3d38eb8c14d194c93fdb609cf4ac69946abc5e4a6fa82"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/env0/terratag/releases/download/v0.1.29/terratag_0.1.29_linux_amd64.tar.gz"
+      sha256 "b6562c80211114547ce64a3bb99c423d365e00b96000bd9cfc4573e8b0287ae3"
+    end
   end
 
   def install
